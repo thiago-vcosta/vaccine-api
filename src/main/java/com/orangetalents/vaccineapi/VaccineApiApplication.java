5773135e4c93adf3999da7129eda00657daa72e8
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-04-09 15:02:11 -0300

    first commit

---
+++ src/main/java/com/orangetalents/vaccineapi/VaccineApiApplication.java
@@ -0,0 +1,13 @@
+package com.orangetalents.vaccineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangetalents/vaccineapi/VaccineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangetalents.vaccineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
